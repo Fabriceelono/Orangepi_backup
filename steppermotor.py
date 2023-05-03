5b2481fc0b69b2d15f595af80492466c2eb5727b
Fabriceelono <r0912443@student.thomasmore.be>
2023-05-04 01:59:56 +0200

    updated

--- steppermotor.py
+++ steppermotor.py
@@ -1,23 +1,34 @@
 import wiringpi
 import time
 
-# Define the number of steps per revolution
+# Define the number of steps per revolution of the stepper motor
 STEPS_PER_REVOLUTION = 2038
 
 # Define the GPIO pins for the stepper motor
-PIN1 = 0  # BCM GPIO 17 Physical 11
-PIN2 = 1  # BCM GPIO 18 ,12
-PIN3 = 2  # BCM GPIO 27, 13
-PIN4 = 3  # BCM GPIO 22,15
+# These are BCM pin numbers, which correspond to physical pin numbers on the Raspberry Pi
+# For example, BCM pin 17 corresponds to physical pin 11 on the Pi's GPIO header
+PIN1 = 17
+PIN2 = 18
+PIN3 = 27
+PIN4 = 22
 
 # Define the step sequence for the stepper motor
+# This is the sequence of signals that need to be sent to the motor's coils to make it rotate one step
+# The sequence is a 2D list where each row represents one step of the motor
+# Each column of the row corresponds to a different coil of the motor
+# The values in each column are either 1 or 0, indicating whether that coil should be energized or not
+# The sequence should be designed so that the motor rotates smoothly and without skipping steps
 SEQUENCE = [[1, 0, 0, 1],
             [1, 0, 0, 0],
             [1, 1, 0, 0],
-            [0, 1, 0, 0]]
+            [0, 1, 0, 0],
+            [0, 1, 1, 0],
+            [0, 0, 1, 0],
+            [0, 0, 1, 1],
+            [0, 0, 0, 1]]
 
 # Set up the GPIO pins
-wiringpi.wiringPiSetup()
+wiringpi.wiringPiSetupGpio()  # Use BCM pin numbers instead of WiringPi pin numbers
 wiringpi.pinMode(PIN1, wiringpi.OUTPUT)
 wiringpi.pinMode(PIN2, wiringpi.OUTPUT)
 wiringpi.pinMode(PIN3, wiringpi.OUTPUT)
@@ -25,20 +36,26 @@ wiringpi.pinMode(PIN4, wiringpi.OUTPUT)
 
 # Function to rotate the stepper motor a specified number of steps
 def rotate(steps, direction, speed):
-    delay_time = 0.01
-    #60.0 / (STEPS_PER_REVOLUTION * speed)
+    # Calculate the delay time between steps based on the desired speed in RPM
+    delay_time = 60.0 / (STEPS_PER_REVOLUTION * speed)
+    
+    # Determine the step sequence to use based on the desired direction of rotation
+    if direction == 'CW':
+        sequence = SEQUENCE
+    else:
+        sequence = SEQUENCE[::-1]  # Reverse the sequence for counterclockwise rotation
+    
+    # Iterate through the specified number of steps and activate the coils of the motor accordingly
     for i in range(steps):
-        for j in range(4):
-            wiringpi.digitalWrite(PIN1, SEQUENCE[j][0])
-            wiringpi.digitalWrite(PIN2, SEQUENCE[j][1])
-            wiringpi.digitalWrite(PIN3, SEQUENCE[j][2])
-            wiringpi.digitalWrite(PIN4, SEQUENCE[j][3])
+        for step in sequence:
+            wiringpi.digitalWrite(PIN1, step[0])
+            wiringpi.digitalWrite(PIN2, step[1])
+            wiringpi.digitalWrite(PIN3, step[2])
+            wiringpi.digitalWrite(PIN4, step[3])
             time.sleep(delay_time)
-    if direction == 'CCW':
-        SEQUENCE.reverse()
 
 # Rotate the stepper motor
-rotate(2038, 'CW', 5) # Rotate 1 revolution clockwise at 5 RPM
-time.sleep(1) # Wait for 1 second
-rotate(2038, 'CCW', 10) # Rotate 1 revolution counterclockwise at 10 RPM
-time.sleep(1) # Wait for 1 second
+rotate(STEPS_PER_REVOLUTION, 'CW', 5)  # Rotate one full revolution clockwise at 5 RPM
+time.sleep(1)  # Wait for 1 second
+rotate(STEPS_PER_REVOLUTION, 'CCW', 10)  # Rotate one full revolution counterclockwise at 10 RPM
+time.sleep(1)  # Wait for 1 second
